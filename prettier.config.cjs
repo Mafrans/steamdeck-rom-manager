a9e6ed2af3f9c391b24e1974fd73d6765c5e97cc
Malte Klüft <malte.kluft@gmail.com>
2022-07-21 18:45:10 +0200

    feat: add tailwind and react-router-dom

---
+++ prettier.config.cjs
@@ -0,0 +1,5 @@
+// prettier.config.js or .prettierrc.js
+module.exports = {
+  plugins: [require('prettier-plugin-tailwindcss')]
+};
+  
---
+++ src/views/HomeView/HomeView.tsx
@@ -0,0 +1,3 @@
+export function HomeView({}): JSX.Element {
+  return <div>Hello World</div>;
+}
