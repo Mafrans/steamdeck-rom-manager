b13e69ca0dc52383f137167d7dd8adf43c6fe439
Malte Klüft <malte.kluft@gmail.com>
2022-08-16 11:13:08 +0200

    feat: add cors

--- src-api/src/main.go
+++ src-api/src/main.go
@@ -3,11 +3,17 @@ package main
 import (
 	"net/http"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	app := gin.Default()
+
+	corsConfig := cors.DefaultConfig()
+	corsConfig.AllowAllOrigins = true
+
+	app.Use(cors.New(corsConfig))
 
 	app.GET("/", func(ctx *gin.Context) {
 		ctx.JSON(http.StatusOK, gin.H{
