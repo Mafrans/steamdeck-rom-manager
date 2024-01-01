c6551c19237b4480afc757f561cacb9368d91457
Mafrans <malte.kluft@gmail.com>
2024-01-01 15:54:21 +0100

    fix: reformat with prettier

--- app/src/main/upload-client/src/pages/Home/index.tsx
+++ app/src/main/upload-client/src/pages/Home/index.tsx
@@ -1,39 +1,33 @@
-import preactLogo from '../../assets/preact.svg';
-import './style.css';
+import { useEffect, useMemo } from "preact/hooks";
+import preactLogo from "../../assets/preact.svg";
+import "./style.css";
+import Uppy from "@uppy/core";
+import DragDrop from "@uppy/drag-drop";
+import Tus from "@uppy/tus";
+
+import "@uppy/core/dist/style.min.css";
+import "@uppy/drag-drop/dist/style.min.css";
+import ProgressBar from "@uppy/progress-bar";
 
 export function Home() {
-	return (
-		<div class="home">
-			<a href="https://preactjs.com" target="_blank">
-				<img src={preactLogo} alt="Preact logo" height="160" width="160" />
-			</a>
-			<h1>Get Started building Vite-powered Preact Apps </h1>
-			<section>
-				<Resource
-					title="Learn Preact"
-					description="If you're new to Preact, try the interactive tutorial to learn important concepts"
-					href="https://preactjs.com/tutorial"
-				/>
-				<Resource
-					title="Differences to React"
-					description="If you're coming from React, you may want to check out our docs to see where Preact differs"
-					href="https://preactjs.com/guide/v10/differences-to-react"
-				/>
-				<Resource
-					title="Learn Vite"
-					description="To learn more about Vite and how you can customize it to fit your needs, take a look at their excellent documentation"
-					href="https://vitejs.dev"
-				/>
-			</section>
-		</div>
-	);
-}
+  useEffect(() => {
+    const uppy = new Uppy({
+      debug: true,
+      autoProceed: true,
+      allowMultipleUploads: true,
+    })
+      .use(DragDrop, { target: "#drag-drop" })
+      .use(ProgressBar, { target: "#progress" })
+      .use(Tus, {
+        endpoint: "/api/upload",
+        onBeforeRequest: async (...args) => console.log(args),
+      });
+  }, []);
 
-function Resource(props) {
-	return (
-		<a href={props.href} target="_blank" class="resource">
-			<h2>{props.title}</h2>
-			<p>{props.description}</p>
-		</a>
-	);
+  return (
+    <div class="home">
+      <div id="drag-drop"></div>
+      <div id="progress"></div>
+    </div>
+  );
 }
--- app/src/main/upload-client/vite.config.ts
+++ app/src/main/upload-client/vite.config.ts
@@ -1,10 +1,10 @@
-import {defineConfig} from 'vite';
-import preact from '@preact/preset-vite';
+import { defineConfig } from "vite";
+import preact from "@preact/preset-vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [preact()],
-    build: {
-        outDir: "../../../build/client"
-    }
+  plugins: [preact()],
+  build: {
+    outDir: "../../../build/client",
+  },
 });
